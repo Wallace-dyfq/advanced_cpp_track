c6427f2642e4b33190a2ad916099fcb4cf297058
wallace <wallace.dyfq@gmail.com>
2014-12-29 21:54:32 -0700

    lab1 ex1

---
+++ lab1/exercise1.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <algorithm>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int RandomNumber() {
+  //  srand(time(NULL));
+  return (std::rand()%100+1); }
+
+int main()
+{
+  vector<int> myvector(100);
+  generate(myvector.begin(), myvector.end(), RandomNumber);
+  copy(myvector.begin(), myvector.end(), ostream_iterator<int>(cout, ", "));
+}
